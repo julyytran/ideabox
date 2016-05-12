923ed52046c06e7a3980d790e671597aa813f4bf
July Tran <julyytran@gmail.com>
2016-05-11 20:38:43 -0600

    truncated bodies

--- app/assets/javascripts/render-ideas.js
+++ app/assets/javascripts/render-ideas.js
@@ -14,8 +14,18 @@ function createIdeaHTML (datum) {
   return "<li id='"
   + datum.id + "' >"
   + "<h4>" + datum.title + "</h4>"
-  + "<p>" + datum.body + "</br>"
+  + "<p>"
+  + truncateBodies(datum.body)
+  + "</br>"
   + datum.quality + "</p>"
   + "<button type='button' name='button' class='btn btn-danger' id='delete'>Delete!</button>"
   + "</li>"
+}
+
+function truncateBodies (body) {
+  if (body.length > 100) {
+    return jQuery.trim(body).substring(0, 100).split(" ").slice(0, -1).join(" ") + "..."
+  } else {
+    return body
+  }
 }
